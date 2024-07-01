67455f7ff00a54c2b236f004acf68017406b6c24
fawdlstty <f@fawdlstty.com>
2024-07-02 00:30:43 +0800

    reimpl transfer

--- src/channel.rs
+++ src/channel.rs
@@ -4,32 +4,131 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Mutex;
 
 #[derive(Debug)]
-pub(crate) struct BaseBuffer<T> {
+pub(crate) struct UnboundedBuffer<T> {
     buf: Vec<T>,
-    rever_poses: Option<HashMap<usize, usize>>,
-    bounded: Option<usize>,
 }
 
-impl<T: Clone + Sized> BaseBuffer<T> {
-    pub fn recv(&mut self, recver_index: usize) -> Option<T> {
-        match self.rever_poses.as_mut() {
-            Some(rever_poses) => {
-                let mut ret = None;
-                {
-                    let cur_pos = rever_poses.entry(recver_index).or_insert(0);
-                    if *cur_pos < self.buf.len() {
-                        ret = Some(self.buf[*cur_pos].clone());
-                        *cur_pos += 1;
-                    }
-                }
-                self.reset_cache_base();
-                ret
-            }
-            None => match self.buf.len() {
-                0 => None,
-                _ => Some(self.buf.remove(0)),
-            },
+impl<T: Clone + Sized> UnboundedBuffer<T> {
+    pub fn send(&mut self, data: T) {
+        self.buf.push(data);
+    }
+
+    pub fn send_items(&mut self, data: Vec<T>) {
+        self.buf.extend(data);
+    }
+
+    pub fn recv(&mut self) -> Option<T> {
+        match self.buf.len() {
+            0 => None,
+            _ => Some(self.buf.remove(0)),
         }
+    }
+
+    pub fn recv_count(&mut self, recv_count: usize, force_count: bool) -> Vec<T> {
+        let mut ret = vec![];
+        let read_count = if recv_count <= self.buf.len() {
+            recv_count
+        } else if !force_count && self.buf.len() > 0 {
+            self.buf.len() - recv_count
+        } else {
+            return ret;
+        };
+        ret.extend(self.buf.drain(0..read_count));
+        ret
+    }
+}
+
+impl<T> UnboundedBuffer<T> {
+    pub fn new() -> Self {
+        Self { buf: vec![] }
+    }
+
+    pub fn len(&self) -> usize {
+        self.buf.len()
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct BoundedBuffer<T> {
+    buf: Vec<T>,
+    bounded: usize,
+}
+
+impl<T: Clone + Sized> BoundedBuffer<T> {
+    pub fn send(&mut self, data: T) {
+        self.buf.push(data);
+        if self.buf.len() > self.bounded {
+            self.buf.remove(0);
+        }
+    }
+
+    pub fn send_items(&mut self, data: Vec<T>) {
+        self.buf.extend(data);
+        if self.buf.len() > self.bounded {
+            self.buf = self.buf.split_at(self.buf.len() - self.bounded).1.to_vec();
+        }
+    }
+
+    pub fn recv(&mut self) -> Option<T> {
+        match self.buf.len() {
+            0 => None,
+            _ => Some(self.buf.remove(0)),
+        }
+    }
+
+    pub fn recv_count(&mut self, recv_count: usize, force_count: bool) -> Vec<T> {
+        let mut ret = vec![];
+        let read_count = if recv_count <= self.buf.len() {
+            recv_count
+        } else if !force_count && self.buf.len() > 0 {
+            self.buf.len() - recv_count
+        } else {
+            return ret;
+        };
+        ret.extend(self.buf.drain(0..read_count));
+        ret
+    }
+}
+
+impl<T> BoundedBuffer<T> {
+    pub fn new(bounded: usize) -> Self {
+        Self {
+            buf: vec![],
+            bounded,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.buf.len()
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct UnboundedDispatchBuffer<T> {
+    buf: Vec<T>,
+    receiver_poses: HashMap<usize, usize>,
+}
+
+impl<T: Clone + Sized> UnboundedDispatchBuffer<T> {
+    pub fn send(&mut self, data: T) {
+        self.buf.push(data);
+    }
+
+    pub fn send_items(&mut self, data: Vec<T>) {
+        self.buf.extend(data);
+    }
+
+    pub fn recv(&mut self, recver_index: usize) -> Option<T> {
+        let mut ret = None;
+        {
+            let cur_pos = self.receiver_poses.entry(recver_index).or_insert(0);
+            if *cur_pos < self.buf.len() {
+                ret = Some(self.buf[*cur_pos].clone());
+                *cur_pos += 1;
+            }
+        }
+        self.reset_cache_base();
+        ret
     }
 
     pub fn recv_count(
@@ -39,65 +138,258 @@ impl<T: Clone + Sized> BaseBuffer<T> {
         force_count: bool,
     ) -> Vec<T> {
         let mut ret = vec![];
-        match self.rever_poses.as_mut() {
-            Some(rever_poses) => {
-                {
-                    let cur_pos = rever_poses.entry(recver_index).or_insert(0);
-                    let read_count = if *cur_pos + recv_count <= self.buf.len() {
-                        recv_count
-                    } else if !force_count && *cur_pos < self.buf.len() {
-                        self.buf.len() - *cur_pos
-                    } else {
-                        return ret;
-                    };
-                    for i in 0..read_count {
-                        ret.push(self.buf[i].clone());
-                    }
-                    *cur_pos += read_count;
-                }
-                self.reset_cache_base();
+        {
+            let cur_pos = self.receiver_poses.entry(recver_index).or_insert(0);
+            let read_count = if *cur_pos + recv_count <= self.buf.len() {
+                recv_count
+            } else if !force_count && *cur_pos < self.buf.len() {
+                self.buf.len() - *cur_pos
+            } else {
+                return ret;
+            };
+            for i in 0..read_count {
+                ret.push(self.buf[i].clone());
             }
-            None => {
-                let read_count = if recv_count <= self.buf.len() {
-                    recv_count
-                } else if !force_count && self.buf.len() > 0 {
-                    self.buf.len() - recv_count
-                } else {
-                    return ret;
-                };
-                ret.extend(self.buf.drain(0..read_count));
-            }
+            *cur_pos += read_count;
         }
+        self.reset_cache_base();
         ret
     }
 }
 
-impl<T> BaseBuffer<T> {
+impl<T> UnboundedDispatchBuffer<T> {
+    pub fn new() -> Self {
+        Self {
+            buf: vec![],
+            receiver_poses: HashMap::new(),
+        }
+    }
+
     pub fn len(&self, recver_index: usize) -> usize {
-        let cur_pos = match &self.rever_poses {
-            Some(rever_poses) => rever_poses.get(&recver_index).cloned().unwrap_or(0),
-            None => 0,
-        };
+        let cur_pos = &self.receiver_poses.get(&recver_index).cloned().unwrap_or(0);
         self.buf.len() - cur_pos
     }
 
+    pub fn new_receiver(&mut self, recver_index: usize) {
+        self.receiver_poses.insert(recver_index, 0);
+    }
+
     pub fn drop_receiver(&mut self, recver_index: usize) {
-        if let Some(rever_poses) = self.rever_poses.as_mut() {
-            rever_poses.remove(&recver_index);
-        }
+        self.receiver_poses.remove(&recver_index);
         self.reset_cache_base();
     }
 
     fn reset_cache_base(&mut self) {
-        if let Some(rever_poses) = self.rever_poses.as_mut() {
-            if let Some(min_pos) = rever_poses.values().min().cloned().take() {
-                if min_pos > 0 {
-                    self.buf.drain(0..min_pos);
-                    for (_, val) in rever_poses.iter_mut() {
-                        *val -= min_pos;
-                    }
+        if let Some(min_pos) = self.receiver_poses.values().min().cloned().take() {
+            if min_pos > 0 {
+                self.buf.drain(0..min_pos);
+                for (_, val) in self.receiver_poses.iter_mut() {
+                    *val -= min_pos;
                 }
             }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct BoundedDispatchBuffer<T> {
+    buf: Vec<T>,
+    receiver_poses: HashMap<usize, usize>,
+    bounded: usize,
+}
+
+impl<T: Clone + Sized> BoundedDispatchBuffer<T> {
+    pub fn send(&mut self, data: T) {
+        self.buf.push(data);
+        if self.buf.len() > self.bounded {
+            self.buf.remove(0);
+            for receiver_pos in self.receiver_poses.values_mut() {
+                if *receiver_pos > 0 {
+                    *receiver_pos -= 1;
+                }
+            }
+        }
+    }
+
+    pub fn send_items(&mut self, data: Vec<T>) {
+        self.buf.extend(data);
+        if self.buf.len() > self.bounded {
+            let remove_count = self.buf.len() - self.bounded;
+            for receiver_pos in self.receiver_poses.values_mut() {
+                *receiver_pos = match *receiver_pos >= remove_count {
+                    true => *receiver_pos - remove_count,
+                    false => 0,
+                };
+            }
+            self.buf = self.buf.split_at(remove_count).1.to_vec();
+        }
+    }
+
+    pub fn recv(&mut self, recver_index: usize) -> Option<T> {
+        let mut ret = None;
+        {
+            let cur_pos = self.receiver_poses.entry(recver_index).or_insert(0);
+            if *cur_pos < self.buf.len() {
+                ret = Some(self.buf[*cur_pos].clone());
+                *cur_pos += 1;
+            }
+        }
+        self.reset_cache_base();
+        ret
+    }
+
+    pub fn recv_count(
+        &mut self,
+        recver_index: usize,
+        recv_count: usize,
+        force_count: bool,
+    ) -> Vec<T> {
+        let mut ret = vec![];
+        {
+            let cur_pos = self.receiver_poses.entry(recver_index).or_insert(0);
+            let read_count = if *cur_pos + recv_count <= self.buf.len() {
+                recv_count
+            } else if !force_count && *cur_pos < self.buf.len() {
+                self.buf.len() - *cur_pos
+            } else {
+                return ret;
+            };
+            for i in 0..read_count {
+                ret.push(self.buf[i].clone());
+            }
+            *cur_pos += read_count;
+        }
+        self.reset_cache_base();
+        ret
+    }
+}
+
+impl<T> BoundedDispatchBuffer<T> {
+    pub fn new(bounded: usize) -> Self {
+        Self {
+            buf: vec![],
+            receiver_poses: HashMap::new(),
+            bounded,
+        }
+    }
+
+    pub fn len(&self, recver_index: usize) -> usize {
+        let cur_pos = self.receiver_poses.get(&recver_index).cloned().unwrap_or(0);
+        self.buf.len() - cur_pos
+    }
+
+    pub fn new_receiver(&mut self, recver_index: usize) {
+        self.receiver_poses.insert(recver_index, 0);
+    }
+
+    pub fn drop_receiver(&mut self, recver_index: usize) {
+        self.receiver_poses.remove(&recver_index);
+        self.reset_cache_base();
+    }
+
+    fn reset_cache_base(&mut self) {
+        if let Some(min_pos) = self.receiver_poses.values().min().cloned().take() {
+            if min_pos > 0 {
+                self.buf.drain(0..min_pos);
+                for (_, val) in self.receiver_poses.iter_mut() {
+                    *val -= min_pos;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum AnyBuffer<T> {
+    UnboundedBuffer(UnboundedBuffer<T>),
+    BoundedBuffer(BoundedBuffer<T>),
+    UnboundedDispatchBuffer(UnboundedDispatchBuffer<T>),
+    BoundedDispatchBuffer(BoundedDispatchBuffer<T>),
+}
+
+impl<T: Clone + Sized> AnyBuffer<T> {
+    pub fn send(&mut self, data: T) {
+        match self {
+            AnyBuffer::UnboundedBuffer(buf) => buf.send(data),
+            AnyBuffer::BoundedBuffer(buf) => buf.send(data),
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.send(data),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.send(data),
+        }
+    }
+
+    pub fn send_items(&mut self, data: Vec<T>) {
+        match self {
+            AnyBuffer::UnboundedBuffer(buf) => buf.send_items(data),
+            AnyBuffer::BoundedBuffer(buf) => buf.send_items(data),
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.send_items(data),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.send_items(data),
+        }
+    }
+
+    pub fn recv(&mut self, recver_index: usize) -> Option<T> {
+        match self {
+            AnyBuffer::UnboundedBuffer(buf) => buf.recv(),
+            AnyBuffer::BoundedBuffer(buf) => buf.recv(),
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.recv(recver_index),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.recv(recver_index),
+        }
+    }
+
+    pub fn recv_count(
+        &mut self,
+        recver_index: usize,
+        recv_count: usize,
+        force_count: bool,
+    ) -> Vec<T> {
+        match self {
+            AnyBuffer::UnboundedBuffer(buf) => buf.recv_count(recv_count, force_count),
+            AnyBuffer::BoundedBuffer(buf) => buf.recv_count(recv_count, force_count),
+            AnyBuffer::UnboundedDispatchBuffer(buf) => {
+                buf.recv_count(recver_index, recv_count, force_count)
+            }
+            AnyBuffer::BoundedDispatchBuffer(buf) => {
+                buf.recv_count(recver_index, recv_count, force_count)
+            }
+        }
+    }
+}
+
+impl<T> AnyBuffer<T> {
+    pub fn new(bounded: Option<usize>, dispatch: bool) -> Self {
+        match (bounded, dispatch) {
+            (Some(bounded), false) => AnyBuffer::BoundedBuffer(BoundedBuffer::<T>::new(bounded)),
+            (None, false) => AnyBuffer::UnboundedBuffer(UnboundedBuffer::<T>::new()),
+            (Some(bounded), true) => {
+                AnyBuffer::BoundedDispatchBuffer(BoundedDispatchBuffer::<T>::new(bounded))
+            }
+            (None, true) => AnyBuffer::UnboundedDispatchBuffer(UnboundedDispatchBuffer::<T>::new()),
+        }
+    }
+
+    pub fn len(&self, recver_index: usize) -> usize {
+        match self {
+            AnyBuffer::UnboundedBuffer(buf) => buf.len(),
+            AnyBuffer::BoundedBuffer(buf) => buf.len(),
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.len(recver_index),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.len(recver_index),
+        }
+    }
+
+    pub fn new_receiver(&mut self, recver_index: usize) {
+        match self {
+            AnyBuffer::UnboundedBuffer(_) => {}
+            AnyBuffer::BoundedBuffer(_) => {}
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.new_receiver(recver_index),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.new_receiver(recver_index),
+        }
+    }
+
+    pub fn drop_receiver(&mut self, recver_index: usize) {
+        match self {
+            AnyBuffer::UnboundedBuffer(_) => {}
+            AnyBuffer::BoundedBuffer(_) => {}
+            AnyBuffer::UnboundedDispatchBuffer(buf) => buf.drop_receiver(recver_index),
+            AnyBuffer::BoundedDispatchBuffer(buf) => buf.drop_receiver(recver_index),
         }
     }
 }
@@ -107,7 +399,7 @@ pub(crate) struct Channel<T> {
     sender_count: AtomicUsize,
     receiver_count: AtomicUsize,
     max_receiver_index: AtomicUsize,
-    buf: Mutex<BaseBuffer<T>>,
+    buf: Mutex<AnyBuffer<T>>,
 }
 
 impl<T> Channel<T> {
@@ -116,14 +408,7 @@ impl<T> Channel<T> {
             sender_count: AtomicUsize::new(1),
             receiver_count: AtomicUsize::new(1),
             max_receiver_index: AtomicUsize::new(1),
-            buf: Mutex::new(BaseBuffer {
-                buf: vec![],
-                rever_poses: match dispatch {
-                    true => Some(vec![(0, 0)].into_iter().collect()),
-                    false => None,
-                },
-                bounded,
-            }),
+            buf: Mutex::new(AnyBuffer::new(bounded, dispatch)),
         })));
         (Sender { chan }, Receiver { chan, index: 0 })
     }
@@ -133,44 +418,17 @@ pub struct Sender<T> {
     chan: NonNull<Channel<T>>,
 }
 
-impl<T> std::fmt::Debug for Sender<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let chan = unsafe { self.chan.as_ref() }.buf.lock().unwrap();
-        f.debug_struct("Receiver")
-            .field("chan.buf.len()", &chan.buf.len())
-            .field("chan.rever_poses", &chan.rever_poses)
-            .field("chan.bounded", &chan.bounded)
-            .finish()
-    }
-}
-
-impl<T> Sender<T> {
+impl<T: Clone + Sized> Sender<T> {
     pub fn send(&self, data: T) {
         let chan: *mut Channel<T> = unsafe { std::mem::transmute(self.chan) };
         let mut buf = unsafe { &(*chan) }.buf.lock().unwrap();
-        match buf.bounded {
-            Some(bounded) => {
-                if buf.buf.len() >= bounded {
-                    buf.buf.remove(0);
-                }
-                buf.buf.push(data);
-            }
-            None => buf.buf.push(data),
-        }
+        buf.send(data);
     }
 
     pub fn send_items(&self, data: Vec<T>) {
         let chan: *mut Channel<T> = unsafe { std::mem::transmute(self.chan) };
         let mut buf = unsafe { &(*chan) }.buf.lock().unwrap();
-        match buf.bounded {
-            Some(bounded) => {
-                for _ in bounded..buf.buf.len() {
-                    buf.buf.remove(0);
-                }
-                buf.buf.extend(data);
-            }
-            None => buf.buf.extend(data),
-        }
+        buf.send_items(data);
     }
 }
 
@@ -203,18 +461,6 @@ pub trait GetTimestampExt {
 pub struct Receiver<T> {
     chan: NonNull<Channel<T>>,
     index: usize,
-}
-
-impl<T> std::fmt::Debug for Receiver<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let chan = unsafe { self.chan.as_ref() }.buf.lock().unwrap();
-        f.debug_struct("Receiver")
-            .field("chan.buf.len()", &chan.buf.len())
-            .field("chan.rever_poses", &chan.rever_poses)
-            .field("chan.bounded", &chan.bounded)
-            .field("index", &self.index)
-            .finish()
-    }
 }
 
 impl<T: Clone + Sized> Receiver<T> {
@@ -257,9 +503,7 @@ impl<T> Clone for Receiver<T> {
             .max_receiver_index
             .fetch_add(1, Ordering::SeqCst);
         let mut buf = unsafe { &(*chan) }.buf.lock().unwrap();
-        if let Some(rever_poses) = buf.rever_poses.as_mut() {
-            rever_poses.insert(index, 0);
-        }
+        buf.new_receiver(index);
         Self {
             chan: self.chan,
             index,
